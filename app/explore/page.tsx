681cbf6ad75749b1f2d411f0f3bf66d0f63282ac
xiaonan <14mingyue@gmail.com>
2026-07-06 17:25:07 +0200

    fix file naming issues

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,7 +1,7 @@
-import BrowseAll from '@/components/explore/BrowseAll';
-import Search from '@/components/explore/Search';
-import YourTopGenres from '@/components/explore/YourTopGenres';
-import React from 'react';
+import BrowseAll from "@/components/explore/BrowseAll";
+import Search from "@/components/explore/Search";
+import YourTopGenres from "@/components/explore/YourTopGenres";
+import React from "react";
 
 const page = () => {
   return (
@@ -10,7 +10,7 @@ const page = () => {
         className="fixed top-[-40vh] w-112.5 h-screen z-0 pointer-events-none"
         style={{
           background:
-            'linear-gradient(to bottom, #06A0B5 0%, #102B2D 46%, #0e0e0e 100%)',
+            "linear-gradient(to bottom, #06A0B5 0%, #102B2D 46%, #0e0e0e 100%)",
         }}
       />
       <div className="z-10 relative">
